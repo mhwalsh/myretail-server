42f0aaf5cf0e833ae7be2b8f625ba537557181b8
Millicent Walsh <walsh.millicent@gmail.com>
2017-09-25 07:58:40 -0500

    start adding more error handling

--- src/modules/productService.js
+++ src/modules/productService.js
@@ -14,13 +14,14 @@ class ProductService {
     }
 
     getProductNameById(prodId) {
+        // type checking here 
         let newURL = this.url.replace(/PRODUCTID/g, parseInt(prodId));
         return rp(newURL, this.options).then((resp) => {
             return JSON.parse(resp).product.item.product_description.title;
         }).catch((err) => {
             // if 404, send specific error message
             if (err.statusCode === 404) {
-                throw new Error('invalid product id');
+                throw new Error(err, 'invalid product id');
             
             } else { // otherwise bubble up
                 throw (err);
--- src/routers/product.js
+++ src/routers/product.js
@@ -22,22 +22,46 @@ router.get('quickCreate/:id/:value/:currency_code', (req, res) => {
 
 router.get('/:id', (req, res) => {
     console.log('in the thign')
-    let productId = req.params.id;
+    // probably should use regex here
+    let productId = parseInt(req.params.id);
+    console.log(productId)
+    // if not a number
+    // if has any weird charaters in it
+    // positive number
+    if(isNaN(productId)){
+        res.status(404).send({message: 'Error invalid product id type'});
+    }
 
     // reliability
     // optimize for speed: cache first, 
     // optimize for accuracy: external service, then to the cache  
-    productService.getProductNameById(productId).then((externalProd) => { 
+    productService.getProductNameById(productId).then((externalProd) => {
         Product.findOne({ product_id: productId }, (err, localProd) => {
+
+            // error querying the local db
+            // id does exist in the external service, but something went wrong
+            if (err) {
+                res.status(404).send({ message: 'Error querying local database' });
+            }
+
             let productComposite = {
                 id: productId,
                 name: externalProd,
                 value: localProd.value,
                 currency_code: localProd.currency_code
             }
-            localProd.name = externalProd;
             res.status(200).send(productComposite);
         });
+    }).catch((err) => {
+        
+        // 404 from external service, id doesn't exist in either system
+        if (err.statusCode === 404) {
+            res.status(404).send({message: 'Error invalid product id'});            
+        } else {
+            // external service is down or other issue
+            console.log(err);
+            res.sendStatus(500);                        
+        }
     });
 });
 
