56c701fa6e0c8ac6182c06330265f78c214910b6
Millicent Walsh <walsh.millicent@gmail.com>
2017-09-24 19:36:20 -0500

    add get by id route

--- src/routers/product.js
+++ src/routers/product.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Product = require('../modules/productSchema');
+const productService = require('../modules/productService');
 
 router.get('quickCreate/:id/:value/:currency_code', (req, res) => {
     console.log(req.params);
@@ -20,9 +21,23 @@ router.get('quickCreate/:id/:value/:currency_code', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    Product.find({ product_id: req.params.id }, (err, product) => {
-        console.log(product);
-        res.send(product);
+    console.log('in the thign')
+    let productId = req.params.id;
+
+    // reliability
+    // optimize for speed: cache first, 
+    // optimize for accuracy: external service, then to the cache  
+    productService.getProductNameById(productId).then((externalProd) => { 
+        Product.findOne({ product_id: productId }, (err, localProd) => {
+            let productComposite = {
+                id: productId,
+                name: externalProd,
+                value: localProd.value,
+                currency_code: localProd.currency_code
+            }
+            localProd.name = externalProd;
+            res.send(productComposite);
+        });
     });
 });
 
