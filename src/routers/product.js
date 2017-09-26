cd9ae4dad73487488fdea609e71cf8539af96b8d
Millicent Walsh <walsh.millicent@gmail.com>
2017-09-25 23:08:21 -0500

    wrap up validate id

--- src/routers/product.js
+++ src/routers/product.js
@@ -21,48 +21,51 @@ router.get('quickCreate/:id/:value/:currency_code', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    console.log('in the thign')
-    // probably should use regex here
-    let productId = parseInt(req.params.id);
-    console.log(productId)
-    // if not a number
-    // if has any weird charaters in it
-    // positive number
-    if(isNaN(productId)){
-        res.status(404).send({message: 'Error invalid product id type'});
-    }
+    console.log('in get product by id route');
 
-    // reliability
-    // optimize for speed: cache first, 
-    // optimize for accuracy: external service, then to the cache  
-    productService.getProductNameById(productId).then((externalProd) => {
-        Product.findOne({ product_id: productId }, (err, localProd) => {
+    if (!isValidId(req.params.id)) {
+        res.status(404).send({ message: 'Error invalid product id type or format' });
 
-            // error querying the local db
-            // id does exist in the external service, but something went wrong
-            if (err) {
-                res.status(404).send({ message: 'Error querying local database' });
+    } else {
+        let productId = parseInt(req.params.id);
+
+        productService.getProductNameById(productId).then((externalProd) => {
+            Product.findOne({ product_id: productId }, (err, localProd) => {
+
+                // product id exists in external service, but failed to query local db
+                if (err) {
+                    res.status(404).send({ message: 'Error querying local database' });
+                }
+
+                let productComposite = {
+                    id: productId,
+                    name: externalProd,
+                    value: localProd.value,
+                    currency_code: localProd.currency_code
+                }
+                res.status(200).send(productComposite);
+            });
+        }).catch((err) => {
+
+            // 404 from external service
+            if (err.statusCode === 404) {
+                res.status(404).send({ message: 'Error invalid product id' });
+            } else {
+                // external service is down or other issue
+                console.log(err);
+                res.sendStatus(500);
             }
-
-            let productComposite = {
-                id: productId,
-                name: externalProd,
-                value: localProd.value,
-                currency_code: localProd.currency_code
-            }
-            res.status(200).send(productComposite);
         });
-    }).catch((err) => {
-        
-        // 404 from external service, id doesn't exist in either system
-        if (err.statusCode === 404) {
-            res.status(404).send({message: 'Error invalid product id'});            
-        } else {
-            // external service is down or other issue
-            console.log(err);
-            res.sendStatus(500);                        
-        }
-    });
+    }
 });
+
+
+/**
+ * 
+ * @param id 
+ */
+function isValidId(id) {
+    return /^[0-9]+$/.test(id);
+}
 
 module.exports = router;
