4fd69d402b5b948a2e1ed7dcc5ddfe9842c6ea91
Millicent Walsh <walsh.millicent@gmail.com>
2017-09-26 21:44:46 -0500

    reorg befores and before eaches

--- test/productService.test.js
+++ test/productService.test.js
@@ -4,29 +4,28 @@ const productService = require('../src/modules/productService');
 
 describe('ProductService', () => {
 
-    beforeEach(function () {
-        nock('http://redsky.target.com')
-            .get('/v2/pdp/tcin/13860428')
-            .reply(200, {
-                product: {
-                    item: {
-                        product_description: {
-                            title: 'The Big Lebowski'
-                        }
-                    }
-                }
-            });
-
-
-        nock('http://redsky.target.com')
-            .get('/v2/pdp/tcin/123')
-            .replyWithError('invalid product id');
-
+    before(() => {
+        // before ProductService disable http request
+        // to prevent calls to external service and 
+        // reliably use nock to intercept requests
         nock.disableNetConnect();
-
     });
 
-    describe('#getProductNameById()', () => {
+    describe('#getProductNameById() happy path', () => {
+        beforeEach(() => {
+            nock('http://redsky.target.com')
+                .get('/v2/pdp/tcin/13860428')
+                .reply(200, {
+                    product: {
+                        item: {
+                            product_description: {
+                                title: 'The Big Lebowski'
+                            }
+                        }
+                    }
+                });
+        });
+
         it('should return product name if called with a valid id', (done) => {
             // valid id for The Big Lebowski
             productService.getProductNameById(13860428).then((name) => {
@@ -42,7 +41,13 @@ describe('ProductService', () => {
                 done();
             });
         });
-
+    });
+    describe('#getProductNameById() bad id error validation', () => {
+        beforeEach(() => {
+            nock('http://redsky.target.com')
+                .get('/v2/pdp/tcin/123')
+                .replyWithError('invalid product id');
+        });
         it('should error nicely when called with invalid id', (done) => {
             productService.getProductNameById(123).catch((err) => {
                 expect(err.message).to.contain('invalid product id');
