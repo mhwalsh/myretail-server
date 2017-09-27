cde8854ad427bf9e0881c9053259886cb764947d
Millicent Walsh <walsh.millicent@gmail.com>
2017-09-26 22:43:12 -0500

    add test for validator and urlformatter

---
+++ test/idValidator.test.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+const isValidId = require('../src/modules/idValidator');
+
+describe('isValidId module', () => {
+    it('should return true when passed string of all numeric characters', () => {
+        expect(isValidId('1234354')).to.be.true;
+    });
+
+    it('should return true when passed an integer', () => {
+        expect(isValidId('1234354')).to.be.true;
+    });
+
+    it('should return false if passed string with any non numeric characters', () => {
+        expect(isValidId('234%e4')).to.be.false;
+    });
+});
