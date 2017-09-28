7baba6242873af429704e93627e0c0210b40d1dc
Millicent Walsh <walsh.millicent@gmail.com>
2017-09-28 08:16:58 -0500

    throw error out of product service and have router catch it

--- src/modules/productService.js
+++ src/modules/productService.js
@@ -32,13 +32,8 @@ class ProductService {
             return rp(newURL, this.options).then((resp) => {
                 return JSON.parse(resp).product.item.product_description.title;
             }).catch((err) => {
-                // if 404 send specific error message
-                if (err.statusCode === 404) {
-                    throw new Error('invalid product id');
-
-                } else { // otherwise bubble up
-                    throw err;
-                }
+                // otherwise bubble up
+                throw err;
             });
         } else {
             throw new Error('invalid product id');
