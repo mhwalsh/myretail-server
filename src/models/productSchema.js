480327d3c8fe651a9fcb14a1615eaa03b5fe7285
Millicent Walsh <walsh.millicent@gmail.com>
2017-09-27 22:26:12 -0500

    get deployed

--- src/models/productSchema.js
+++ src/models/productSchema.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = require('bluebird');
 
-mongoose.connect('mongodb://localhost:27017/productDb');
+if(process.env.MONGODB_URI !== undefined) {
+    console.log('env connection string');
+    connectionString = process.env.MONGODB_URI;
+    pg.defaults.ssl = true;
+} else {
+    connectionString = 'mongodb://localhost:27017/productDb';
+}
+
+mongoose.connect(connectionString);
 
 let productSchema = new Schema({
     product_id: {type: 'number', require: true, unique: true},
